1da4f1090d9adc27fe817e72c4e11f58c231f561
bhanu9127 <122633510+bhanu9127@users.noreply.github.com>
2023-02-15 12:04:26 +0530

    Add files via upload

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "SRN:PES1UG20CS236 I have successfully built and run\n";
+  return 0;
+}
